ddc17a44dc5db60251db097496f7fe2ae05e6a68
Brian Parker <brian.parker@hotmail.com.au>
2021-09-17 12:53:20 +1000

    Display seeded user roles

--- DynamicRolesPolicy/Client/Shared/TestUsers.razor.cs
+++ DynamicRolesPolicy/Client/Shared/TestUsers.razor.cs
@@ -3,10 +3,10 @@ namespace DynamicRolesPolicy.Client.Shared
     public partial class TestUsers
     {
         string[] users = new[] {
-            "abigail@abigail.com",
-            "brian@brian.com",
-            "cruz@cruz.com",
-            "serenity@serenity.com",
-            "tyler@tyler.com" };
+            "abigail@abigail.com (Moderator)",
+            "brian@brian.com (Administrator)",
+            "cruz@cruz.com (Moderator)",
+            "serenity@serenity.com (TenantAdministrator)",
+            "tyler@tyler.com (Moderator,TenantAdministrator)" };
     }
 }
