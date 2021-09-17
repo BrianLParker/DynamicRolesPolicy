a71d5deb5b8890dba18da9d898aa12e5e03134cf
Brian Parker <brian.parker@hotmail.com.au>
2021-09-17 13:15:42 +1000

    code rub

--- DynamicRolesPolicy/Client/Shared/TestUsers.razor.cs
+++ DynamicRolesPolicy/Client/Shared/TestUsers.razor.cs
@@ -1,12 +1,11 @@
-namespace DynamicRolesPolicy.Client.Shared
+namespace DynamicRolesPolicy.Client.Shared;
+
+public partial class TestUsers
 {
-    public partial class TestUsers
-    {
-        string[] users = new[] {
+    string[] users = new[] {
             "abigail@abigail.com (Moderator)",
             "brian@brian.com (Administrator)",
             "cruz@cruz.com (Moderator)",
             "serenity@serenity.com (TenantAdministrator)",
             "tyler@tyler.com (Moderator,TenantAdministrator)" };
-    }
 }
